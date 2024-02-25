63cd610e9b589fc009b65e0b394b2301273deb05
ygursu <yasemingrs@gmail.com>
2024-02-25 21:52:57 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp91/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedenkenarlarigirilendikdortgenincevresi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kisakenar, uzunkenar, cevre;
+
+            Console.WriteLine("Lütfen dikdörtgenin kısa kenarını giriniz");
+            kisakenar = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen dikdörtgenin uzun kenarını giriniz");
+            uzunkenar = Int32.Parse(Console.ReadLine());
+
+            cevre = kisakenar + uzunkenar + kisakenar + uzunkenar;
+
+            Console.WriteLine("Dikdörtgenin çevresi={0}", cevre);
+            Console.ReadLine();
+
+        }
+    }
+}
